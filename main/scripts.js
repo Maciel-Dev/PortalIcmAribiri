8ff99e28eaa826af885db59c0167dbcba94e7635
Joao Maciel <joaovitormaciel@infosiscs.com>
2022-08-16 21:49:44 -0300

    Function and SpreadSheetAPi

--- main/scripts.js
+++ main/scripts.js
@@ -1,5 +1,20 @@
-var mainTitle = document.querySelector("#princ");
-
-document.addEventListener("DOMContentLoaded", function(event) {
-    console.log("DOM completamente carregado e analisado");
-  });
+function SubForm(){
+  var resposta = "";
+  var lines = document.querySelector("#respostaFormulário").innerHTML;
+  for(var i = 0; i < lines.length; i++)
+  {
+    resposta = resposta + `\n`;
+  }
+  fetch("https://api.apispreadsheets.com/data/1l9Dh561by71Ppr7/", {
+	method: "POST",
+	body: JSON.stringify({"data": {"cpf":document.querySelector("#cpfForm").value,"name":document.querySelector("#nameForm").value,"answer":resposta}}),
+}).then(res =>{
+	if (res.status === 201){
+		// SUCCESS
+    alert("OKAY");
+	}
+	else{
+		// ERROR
+	}
+})
+}
