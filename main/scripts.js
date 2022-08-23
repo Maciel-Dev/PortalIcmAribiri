66be0b30c8e3907847bb328b81a9efd8c9576f7d
Joao Maciel <joaovitormaciel@infosiscs.com>
2022-08-22 21:40:44 -0300

    21-08

--- main/scripts.js
+++ main/scripts.js
@@ -1,13 +1,7 @@
 function SubForm(){
-  var resposta = "";
-  var lines = document.querySelector("#respostaFormulário").innerHTML;
-  for(var i = 0; i < lines.length; i++)
-  {
-    resposta = resposta + `\n`;
-  }
   fetch("https://api.apispreadsheets.com/data/1l9Dh561by71Ppr7/", {
 	method: "POST",
-	body: JSON.stringify({"data": {"cpf":document.querySelector("#cpfForm").value,"name":document.querySelector("#nameForm").value,"answer":resposta}}),
+	body: JSON.stringify({"data": {"cpf":document.querySelector("#cpfForm").value,"name":document.querySelector("#nameForm").value,"answer":document.querySelector("#respostaFormulário").value}}),
 }).then(res =>{
 	if (res.status === 201){
 		// SUCCESS
